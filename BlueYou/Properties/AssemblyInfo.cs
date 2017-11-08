8ee47838a263943fbbb1d3fc83c8d2c7c57a96fe
sangmanla <sangmanla@naver.com>
2017-11-08 14:03:00 -0500

    Add files via upload

--- BlueYou/Properties/AssemblyInfo.cs
+++ BlueYou/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FoodDiary")]
+[assembly: AssemblyTitle("MealDiary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("FoodDiary")]
+[assembly: AssemblyProduct("MealDiary")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
